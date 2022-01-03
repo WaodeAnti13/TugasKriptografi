f0c4d35e502d44410396f61cc017fc5c7c0669f4
WaodeAnti13 <58122825+WaodeAnti13@users.noreply.github.com>
2022-01-03 21:22:18 +0800

    Add files via upload

---
+++ TugasKriptografi_E1E1119083-Waode Anti.py
@@ -0,0 +1,33 @@
+import os
+from hashlib import *
+from tkinter import Tk     
+from tkinter.filedialog import askopenfilename
+
+print("Pililah  file yang ingin di encrypt / decrypt")
+
+
+Tk().withdraw() 
+filename = askopenfilename() 
+print(filename)
+
+
+Fin = filename
+Fout = str(input("Masukan nama file output : "))
+
+K = str(input("Masukan kata kunci : "))
+
+ShaKey = sha256(K.encode('utf-8')).digest()
+print(filename)
+
+
+with open(Fin, 'rb') as Fin:
+    with open (Fout, 'wb') as Fout:
+        i = 0
+    while Fin.peek():
+        c = ord(Fin.read(1))
+        j = i % len(ShaKey)
+        b = bytes([c^ShaKey[j]])
+        Fout.write(b)
+        i = i + 1
+print("File encrypted !")
+
